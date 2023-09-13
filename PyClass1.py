3edc411b4a372fc378f711c0a0ca6849ca390f4b
Dário <86432208+dario-gms@users.noreply.github.com>
2023-09-13 15:03:05 -0300

    First python code
    hello world, write your name and print

---
+++ PyClass1.py
@@ -0,0 +1,10 @@
+# Create a Python script that read the name of one person and show a welcome message.
+
+entry = input("Type your name: \n")
+print("Hello, {}! Pleasure to meet you!".format(entry))
+
+# create a program that write "Hello, world" in the screen.
+
+print("Hello, world!")
+msg = "Hello, world!"
+print(msg)
