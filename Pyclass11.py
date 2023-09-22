9dac6d515768ceef420fbfe90f9a3bd936a3886c
Dário <86432208+dario-gms@users.noreply.github.com>
2023-09-22 11:57:22 -0300

    predecessor and successor number

---
+++ Pyclass11.py
@@ -0,0 +1,5 @@
+# make a program the read a int number and show his successor and predecessor
+
+n = int(input("Insert a number: \n"))
+print("The predecessor number of {} is: {}.".format(n, n - 1))
+print("The successor number of {} is: {}.".format(n, n + 1))
